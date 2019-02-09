1840b763d21fdc3ac7474331ae84ea89c86cca40
Shaun <torianharris917@gmail.com>
2019-02-09 18:38:23 -0500

    feat/check guesses

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const inquirer = require("inquirer");
 let randWords = ["talked", "sea", "rice", "yearn", "zucchini"];
 let currentWord = new word();
 
+const maxGuesses = 10;
+let guessesLeft = maxGuesses;
+
 function createWord() {
     let rand = randWords[Math.floor(Math.random() * randWords.length)];
     currentWord.splitLetters(rand);
@@ -22,14 +25,48 @@ function requestGuess() {
         }
     }])
         .then(function (res) {
+            checkGuess(res.guess);
             currentWord.letters.map(x => x.compareChar(res.guess));
             currentWord.displayWord();
-            requestGuess();
+            if (checkForEndGame()) {
+                console.log("You win!");
+                //TODO and replay inquirer
+            }
+            else {
+                requestGuess();
+            }
         })
         .catch(function (err) {
             throw err;
         })
 }
 
-createWord();
-requestGuess();
+function checkGuess(input) {
+    for (i = 0; i < currentWord.letters.length; i++) {
+        if (currentWord.letters[i].char === input) {
+            if (currentWord.letters[i].guessed === true) {
+                console.log("You already guessed that one.");
+            }
+            else {
+                console.log("Nice guess!");
+            }
+            return;
+        }
+    }
+    console.log("Sorry that's wrong.");
+}
+
+function checkForEndGame() {
+    for (i = 0; i < currentWord.letters.length; i++) {
+        if (currentWord.letters[i].guessed === false)
+            return false;
+    }
+    return true;
+}
+
+function startGame() {
+    createWord();
+    requestGuess();
+}
+
+startGame();
